8832c252d37887c0e140a06d3c760aa05b9d9d65
Andrej Kling <boxingprofi@web.de>
2021-01-17 14:21:09 +0100

    Initial Commit

---
+++ MyWay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyWay
+//
+//  Created by Andrej Kling on 17.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyWay/MyWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWayApp.swift
+//  MyWay
+//
+//  Created by Andrej Kling on 17.01.21.
+//
+
+import SwiftUI
+
+@main
+struct MyWayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
